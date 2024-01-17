3fc1273a2be65d675049d98fad451b8019132a21
PhlegmaTREEc <phlegmatreec@pm.me>
2024-01-17 23:53:16 +0100

    qt things

--- system/configuration.nix
+++ system/configuration.nix
@@ -37,7 +37,7 @@
       GDK_BACKEND = "wayland,x11";
       CLUTTER_BACKEND = "wayland";
       QT_QPA_PLATFORM = "wayland;xcb";
-      QT_QPA_PLATFORMTHEME = "qt6ct";
+      QT_QPA_PLATFORMTHEME = "qt5ct";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       MOZ_ENABLE_WAYLAND = "1";
@@ -191,7 +191,10 @@
     };
     udev = {
       enable = true;
-      };
+      packages = with pkgs; [
+        bazecor
+      ];
+    };
     upower.enable = true;
   };
 

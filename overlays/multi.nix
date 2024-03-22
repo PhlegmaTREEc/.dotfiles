33b328bd245fd1daa71827c436eaa05cc42070e1
PhlegmaTREEc <phlegmatreec@pm.me>
2024-03-22 21:29:18 +0100

    multiviewer overlay to new version

--- overlays/multi.nix
+++ overlays/multi.nix
@@ -2,10 +2,10 @@ self: super:
 {
   myMulti = super.multiviewer-for-f1.overrideAttrs (old: rec {
     pname = "multiviewer-for-f1";
-    version = "1.31.4";
+    #version = "1.31.7";
     src = super.fetchurl {
-      url = "https://releases.multiviewer.app/download/154454223/multiviewer-for-f1_${version}_amd64.deb";
-      sha256 = "sha256-HlKs4GMyHLLfQUGSsX6O8ImJ9GCoSc9KAHEuDESohvw=";
+      url = "https://releases.multiviewer.dev/download/155752210/multiviewer-for-f1_1.37.7_amd64.deb";
+      sha256 = "sha256-Tnr5Lvzoswhbvhs9SOprP2TcPbr6SP1uR3OqtnqiPrA=";
     };
   });
 }

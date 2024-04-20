f2a9e0dbb009d72df0c620429a42f3188204ae32
PhlegmaTREEc <phlegmatreec@pm.me>
2024-04-20 19:47:15 +0200

    usys script updated to reflect recent changes

--- usys.sh
+++ usys.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+
 pushd ~/.dotfiles
+
+if [ ! -z $1 ]; then
+	export HOST=$1
+else
+	export HOST=$(hostname)
+fi
+
 nix flake update
-sudo nixos-rebuild switch --flake .#
+sudo nixos-rebuild --flake .#$HOST switch
+
+#nix flake update
+#sudo nixos-rebuild switch --flake .#
+
 popd

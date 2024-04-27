1bc3b1a32629aebe388091d86f97075d1dd643d4
PhlegmaTREEc <phlegmatreec@pm.me>
2024-04-27 17:44:31 +0200

    flexible update lab test

--- update.sh
+++ update.sh
@@ -10,10 +10,16 @@ if [ $USERNAME = "ptc" ]; then
 	else
 		export HOST=$(hostname)
 	fi
-	echo "pre-ok"
+	echo "pre-ok nixmain"
 	nix flake update
 	sudo nixos-rebuild switch --flake .#$HOST
-	echo "ok"
+	echo "ok nixmain"
+elif [ $USERNAME = "ptclab" ]; then
+	echo "pre-ok lab"
+	nix flake update
+	nix build .#homeConfigurations.ptclab.activationPackage
+	./result/activate
+	echo "ok lab"
 else
-	echo "notok"
+	echo "unknow configuration"
 fi

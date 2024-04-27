52300b6e8cf61cc5c95bebdb2d3d163a37a463b9
PhlegmaTREEc <phlegmatreec@pm.me>
2024-04-27 14:14:18 +0000

    lock ignored and test home.nix nix settings

--- home/lab/home.nix
+++ home/lab/home.nix
@@ -16,6 +16,11 @@
   nix = {
     package = pkgs.nix;
     settings.experimental-features = [ "nix-command" "flakes" ];
+    gc = {
+      automatic = true;
+      frequency = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
   
   home.sessionVariables = {

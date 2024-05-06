d6d359268ec78e439b70c3870dc007f235cdf783
PhlegmaTREEc <phlegmatreec@pm.me>
2024-05-06 14:15:41 +0200

    minifuse enabled in easy effects, scripts to handle the channels adjusted

--- home/scripts/volume/set_snowball_when_inactive.sh
+++ home/scripts/volume/set_snowball_when_inactive.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 wpctl set-volume $(pw-cli i alsa_input.usb-BLUE_MICROPHONE_Blue_Snowball_201509-00.analog-stereo | grep -oP 'id: \K\w+') 0.8
+wpctl set-volume $(pw-cli i alsa_input.usb-ARTURIA_ArturiaMsd-00.analog-stereo | grep -oP 'id: \K\w+') 1.5
 wpctl set-mute $(pw-cli i alsa_input.usb-BLUE_MICROPHONE_Blue_Snowball_201509-00.analog-stereo | grep -oP 'id: \K\w+') toggle
+wpctl set-mute $(pw-cli i alsa_input.usb-ARTURIA_ArturiaMsd-00.analog-stereo | grep -oP 'id: \K\w+') toggle
+pw-link "alsa_input.usb-ARTURIA_ArturiaMsd-00.analog-stereo:capture_FL" "ee_sie_rnnoise:input_FL"
+pw-link "alsa_input.usb-ARTURIA_ArturiaMsd-00.analog-stereo:capture_FL" "ee_sie_rnnoise:input_FR"
+pw-link --disconnect "alsa_input.usb-ARTURIA_ArturiaMsd-00.analog-stereo:capture_FR" "ee_sie_rnnoise:input_FR"
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 0.8

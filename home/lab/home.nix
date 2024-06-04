8f74e24b17bf72138dd4e5de1c7dd07af742c150
PhlegmaTREEc <phlegmatreec@pm.me>
2024-06-04 18:58:10 +0200

    nix settings removed

--- home/lab/home.nix
+++ home/lab/home.nix
@@ -11,15 +11,5 @@
       ../core/default.nix
     ];
 
-  nix = {
-    package = pkgs.nix;
-    settings.experimental-features = [ "nix-command" "flakes" ];
-    gc = {
-      automatic = true;
-      frequency = "weekly";
-      options = "--delete-older-than 7d";
-    };
-  };
-  
   programs.home-manager.enable = true;
 }

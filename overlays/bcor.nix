b56dc15c2ef8eb0a7ff6ef38a76b908cce32ac89
PhlegmaTREEc <phlegmatreec@pm.me>
2024-06-07 22:10:25 +0200

    trying overlay for bazecor

--- overlays/bcor.nix
+++ overlays/bcor.nix
@@ -4,7 +4,7 @@ self: super:
     pname = "bazecor";
     src = super.fetchurl {
       url = "https://github.com/Dygmalab/Bazecor/releases/download/v1.4.0-rc4/Bazecor-1.4.0-rc.4-x64.AppImage";
-      sha256 = "";
+      sha256 = "sha256-ArbWUiL/eLjJYMS1yHBgDplHAUQcZ0EYWftnZuSwGH4=";
     };
   });
 }

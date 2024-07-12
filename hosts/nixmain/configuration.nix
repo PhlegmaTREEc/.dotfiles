7737ec6d11446364c984c5f0f750ee11191b944d
PhlegmaTREEc <phlegmatreec@pm.me>
2024-07-12 18:56:01 +0200

    some option renamed for nix

--- hosts/nixmain/configuration.nix
+++ hosts/nixmain/configuration.nix
@@ -72,10 +72,9 @@
       enable = true;
       enableGraphical = true;
     };
-    opengl = {
+    graphics = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      enable32Bit = true;
       extraPackages = [ pkgs.amdvlk ];
       extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
     };

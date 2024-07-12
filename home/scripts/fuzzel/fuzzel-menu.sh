fd08bb7e16b67fafcf80f6387e22d458154e901d
PhlegmaTREEc <phlegmatreec@pm.me>
2024-07-12 18:55:35 +0200

    new monitors added and additional option to fully (dis)able the monitors added

--- home/scripts/fuzzel/fuzzel-menu.sh
+++ home/scripts/fuzzel/fuzzel-menu.sh
@@ -3,24 +3,40 @@
 # Options for menu
 rsn="󱩍 Night light"
 rsd="󰛨 Day light"
-dp2on="󰍺 DP-2 On"
-dp2off="󰶐 DP-2 Off"
+monon="󰍺 ON"
+monoff="󰶐 OFF"
+monen="󰍺 ENABLE"
+mondis="󰶐 DISABLE"
 
 # Get answer from user via wofi
 selected_option=$(echo "$rsn
 $rsd
-$dp2off
-$dp2on" | fuzzel -d -l 4 -w 20 -p "Menu"\ )
+$monen
+$mondis
+$monon
+$monoff" | fuzzel -d -l 6 -w 20 -p "Menu"\ )
 
 # Do something based on selected option
 if [ "$selected_option" == "$rsn" ]; then
 	hyprctl dispatch exec "gammastep -l 0:0 -o -b 0.8:0.8 -O 4500"
 elif [ "$selected_option" == "$rsd" ]; then
 	hyprctl dispatch exec "pkill gammastep"
-elif [ "$selected_option" == "$dp2on" ]; then
+elif [ "$selected_option" == "$monon" ]; then
+	hyprctl dispatch dpms on HDMI-A-1
 	hyprctl dispatch dpms on DP-2
-elif [ "$selected_option" == "$dp2off" ]; then
+	hyprctl dispatch dpms on DP-3
+elif [ "$selected_option" == "$monoff" ]; then
+	hyprctl dispatch dpms off HDMI-A-1
 	hyprctl dispatch dpms off DP-2
+	hyprctl dispatch dpms off DP-3
+elif [ "$selected_option" == "$monen" ]; then
+	sed -i "s/^monitor = DP-2,disable/#monitor = DP-2,disable/" /home/ptc/.dotfiles/home/dotconfig/hypr/hyprland.conf
+	sed -i "s/^monitor = DP-3,disable/#monitor = DP-3,disable/" /home/ptc/.dotfiles/home/dotconfig/hypr/hyprland.conf
+	sed -i "s/^monitor = HDMI-A-1,disable/#monitor = HDMI-A-1,disable/" /home/ptc/.dotfiles/home/dotconfig/hypr/hyprland.conf
+elif [ "$selected_option" == "$mondis" ]; then
+	sed -i "s/^#monitor = DP-2,disable/monitor = DP-2,disable/" /home/ptc/.dotfiles/home/dotconfig/hypr/hyprland.conf
+	sed -i "s/^#monitor = DP-3,disable/monitor = DP-3,disable/" /home/ptc/.dotfiles/home/dotconfig/hypr/hyprland.conf
+	sed -i "s/^#monitor = HDMI-A-1,disable/monitor = HDMI-A-1,disable/" /home/ptc/.dotfiles/home/dotconfig/hypr/hyprland.conf
 else
 	echo "No match"
 fi

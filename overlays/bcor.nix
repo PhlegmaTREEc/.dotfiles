67bd34010c8bb638a2cd97f4b046c2b66b68b436
PhlegmaTREEc <phlegmatreec@pm.me>
2024-07-20 21:13:10 +0200

    removed test garbage

--- overlays/bcor.nix
+++
@@ -1,9 +0,0 @@
-final: prev:
-{
-  bcor = prev.bazecor.overrideAttrs (old: rec {
-    src = prev.fetchurl {
-      url = "https://github.com/Dygmalab/Bazecor/releases/download/v1.4.0-rc4/Bazecor-1.4.0-rc.4-x64.AppImage";
-      sha256 = "sha256-ArbWUiL/eLjJYMS1yHBgDplHAUQcZ0EYWftnZuSwGH4=";
-    };
-  });
-}

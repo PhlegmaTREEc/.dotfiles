1ab8d38872874a6d726e33065b5958953fbe1567
PhlegmaTREEc <phlegmatreec@pm.me>
2024-07-20 21:14:03 +0200

    removed lab systems from update.sh

--- update.sh
+++ update.sh
@@ -9,16 +9,11 @@ if [ $HOST = "nixmain" ]; then
 	nix flake update
 	sudo nixos-rebuild switch --flake .#$HOST
 	echo "Update of nixmain Desktop finished"
-elif [[ $HOST = nixlxc* ]]; then
-	echo "Update of $HOST started"
-	nix flake update
-	sudo nixos-rebuild switch --flake .#nixlxc --impure
-	echo "Update of $HOST finished"
-elif [[ $HOST = nixvm* ]]; then
-	echo "Update of $HOST started"
-	nix flake update -I ./flake-lab
-	sudo nixos-rebuild switch --flake './installer#$HOST' --impure
-	echo "Update of $HOST finished"
+#elif [[ $HOST = nixlxc* ]]; then
+#	echo "Update of $HOST started"
+#	nix flake update
+#	sudo nixos-rebuild switch --flake .#nixlxc --impure
+#	echo "Update of $HOST finished"
 else
 	echo "unknow configuration"
 fi

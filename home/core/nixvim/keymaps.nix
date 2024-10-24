f53bce3733e3e98e0410a67c4627c9537ea5c287
PhlegmaTREEc <phlegmatreec@pm.me>
2024-10-24 17:45:55 +0200

    nixvim step three - keymaps and treesitter

--- home/core/nixvim/keymaps.nix
+++ home/core/nixvim/keymaps.nix
@@ -1,8 +1,37 @@
-{
+{ config, lib, ...}: {
   programs.nixvim = {
     globals = {
       mapleader = "\\";
       maplocalleader = "\\";
     };
+    
+    keymaps = let
+      normal = 
+        lib.mapAttrsToList
+        (key: action: {
+          mode = "n";
+          inherit action key;
+        })
+        {
+          # move current line up/down
+          # M = Alt key
+          "<M-k>" = ":move-2<CR>";
+          "<M-j>" = ":move+<CR>";
+        };
+      visual =
+        lib.mapAttrsToList
+        (key: action: {
+          mode = "v";
+          inherit action key;
+        })
+        {
+          # move selected line / block of text in visual mode
+          "K" = ":m '<-2<CR>gv=gv";
+          "J" = ":m '>+1<CR>gv=gv";
+        };
+    in
+      config.nixvim.helpers.keymaps.mkKeymaps
+      {options.silent = true;}
+      (normal ++ visual);
   };
 }

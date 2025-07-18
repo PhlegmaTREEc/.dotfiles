df83c06ca22e7e489d1f4af072c227aeeb73bcfb
PhlegmaTREEc <phlegmatreec@pm.me>
2025-07-18 20:58:53 +0200

    flatpak list for opensuse prep

---
+++ opensuse/apps_flatpak.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+flatpak install \
+app.zen_browser.zen \
+com.usebottles.bottles \
+org.gnome.Calculator \
+dev.geopjr.Collision\
+io.github.dimtpap.coppwr \
+com.discordapp.Discord \
+com.github.wwmm.easyeffects \
+com.github.tchx84.Flatseal \
+org.gimp.GIMP \
+io.gitlab.adhami3310.Impression \
+com.obsproject.Studio\
+com.heroicgameslauncher.hgl \
+org.kde.krita \
+com.hunterwittenborn.Celeste \
+org.libreoffice.LibreOffice \
+org.gnome.Loupe \
+io.github.diegoivan.pdf_metadata_editor \
+net.lutris.Lutris\
+org.kde.kdenlive\
+om.vysp3r.ProtonPlus \
+org.gnome.Loupe \
+com.github.xournalpp.xournalpp\
+com.github.ahrm.sioyek \
+io.gitlab.adhami3310.Converter \
+org.soundconverter.SoundConverter \
+org.gnome.SoundRecorder \
+org.nickvision.tubeconverter \
+org.upscayl.Upscayl \
+io.github.flattool.Warehouse

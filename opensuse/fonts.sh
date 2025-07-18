c0280a25192bf82486646a71449a5e4e15b5b560
PhlegmaTREEc <phlegmatreec@pm.me>
2025-07-18 20:38:45 +0200

    opensuse prep fonts, make all scripts executable

---
+++ opensuse/fonts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd ~
+mkdir fonts
+cd fonts
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/AdwaitaMono.zip"
+unzip -a AdwaitaMono.zip JetBrainsMono.zip
+sudo mkdir -p /usr/local/share/fonts
+sudo mv *.{ttf,otf} /usr/local/share/fonts
+cd ~
+rm -r fonts
+echo "fonts done"
